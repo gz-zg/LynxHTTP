988d4dddc4fe8667d2ccb07976ca5a7519b60c91
gz_zg <2059361484@qq.com>
2025-06-12 17:38:45 +0000

    fix: 大改 TcpServer 模块，将多个成员函数合并成一个 start 成员函数

--- include/LynxHTTP/TcpServer.hpp
+++ include/LynxHTTP/TcpServer.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+#include <functional>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include "LynxHTTP/InetAddress.hpp"
+#include "LynxHTTP/EventLoop.hpp"
 
 class TcpServer {
 public:
     TcpServer();                                 // 创建一个 TCP + IPv4 的套接字 sockfd_
     ~TcpServer();                                // 释放这个 TCP + IPv4 的套接字 sockfd_
 
-    void bind_listen(const InetAddress& addr);   // 给 sockfd_ 绑定到一个本地地址(IPv4 + 端口), 并监听来自客户端的 TCP + IPv4 的连接
-    int accept(InetAddress* peer_addr);          // 从监听套接字 sockfd_ 的监听队列中接收客户端的连接请求,返回与客户端通信的新套接字的文件描述符
-    int fd();                                    // 返回 sockfd_
+    void start(int port);
 
 private:
-    int sockfd_;                                 // TCP 服务器的 套接字
+    EventLoop evloop_;                           // TCP 服务器的 事件循环
+    int listen_fd_;                              // TCP 服务器的 套接字
+    std::function<void()> accept_handler_;       // TCP 服务器的 accept处理器, 它内部封装了当前类中的 handle_accept 成员函数
+    
+    void handle_accept();
 };
--- src/TcpServer.cpp
+++ src/TcpServer.cpp
@@ -1,39 +1,58 @@
 #include "LynxHTTP/TcpServer.hpp"
 #include <sys/socket.h>
 #include <unistd.h>
+#include "LynxHTTP/InetAddress.hpp"
+#include <iostream>
 
+// 创建一个 TCP + IPv4 的套接字 listen_fd_
+TcpServer::TcpServer()
+{
+    listen_fd_ = -1;
+}
 
-// 创建一个 TCP + IPv4 的套接字 sockfd_
-TcpServer::TcpServer(){
-    sockfd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+// 释放这个 TCP + IPv4 的套接字 listen_fd_
+TcpServer::~TcpServer() 
+{
+    if(listen_fd_ != -1)
+    {
+        ::close(listen_fd_);
+    }
+}
 
-    // 给 sockfd_ 设置选项
+void TcpServer::start(int port)
+{
+    listen_fd_ = ::socket(AF_INET, SOCK_STREAM, 0);
+    
+    // 给 listen_fd_ 设置选项
     int val = 1; // val 仅在第一次设置时有效，后续修改 val 的值不会影响已绑定的套接字选项
-    setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)); // 使服务器程序在重启后可以立马绑定到它之前使用的地址[IP:端口]
-}
+    setsockopt(listen_fd_, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)); // 使服务器程序在重启后可以立马绑定到它之前使用的地址[IP:端口]
 
-// 释放这个 TCP + IPv4 的套接字 sockfd_
-TcpServer::~TcpServer() {
-    ::close(sockfd_);
-}
-
-// 给 sockfd_ 绑定到一个本地地址(IPv4 + 端口), 并监听来自客户端的 TCP + IPv4 的连接
-void TcpServer::bind_listen(const InetAddress& addr) {  // const InetAddress& 只能调用 const 成员函数
-    if(::bind(sockfd_, addr.ptr_const(), addr.size()) < 0)
+    // 给 listen_fd_ 绑定到一个本地地址(IPv4 + 端口), 并监听来自客户端的 TCP + IPv4 的连接
+    InetAddress addr(port);
+    if(::bind(listen_fd_, addr.ptr_const(), addr.size()) < 0)
     {
         // [失败处理]之后再写吧
     }
-    ::listen(sockfd_, SOMAXCONN);
+    ::listen(listen_fd_, SOMAXCONN);
+
+    // 给 listen_fd_ 封装一个函数作为回调函数
+    accept_handler_ = [this]()->void { this->handle_accept(); };
+    
+    // 将 listen_fd_ 添加到 事件循环 中
+    evloop_.add_event(listen_fd_, EPOLLIN, &accept_handler_);
+
+    std::cout << "Server started on port: " << port << std::endl;
 }
 
-// 从监听套接字 sockfd_ 的监听队列中接收客户端的连接请求,返回与客户端通信的新套接字的文件描述符
-int TcpServer::accept(InetAddress* peer_addr) {
-    socklen_t peer_addr_len = peer_addr->size();
-    int connfd = ::accept(sockfd_, peer_addr->ptr(), &peer_addr_len); 
-    return connfd;
+void TcpServer::handle_accept()
+{
+
 }
 
-// 返回 sockfd_
-int TcpServer::fd() {
-    return sockfd_;
-}
+// // 从监听套接字 listen_fd_ 的监听队列中接收客户端的连接请求,返回与客户端通信的新套接字的文件描述符
+// int TcpServer::accept(InetAddress* peer_addr) 
+// {
+//     socklen_t peer_addr_len = peer_addr->size();
+//     int connfd = ::accept(listen_fd_, peer_addr->ptr(), &peer_addr_len); 
+//     return connfd;
+// }
