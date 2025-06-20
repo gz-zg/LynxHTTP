4646c93e2517767553afaffc0eeba77eba57763a
gz_zg <2059361484@qq.com>
2025-06-20 18:59:42 +0000

    problem: 不知道为什么当http服务器监听6666端口时，端口转发到windows后，windows中的浏览器无法与http服务器建立连接，但是监听其他端口貌似都有效

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 int main() 
 {
     TcpServer server;
-    server.start(6666, [](std::shared_ptr<TcpConn> conn)->void {
+    std::cout << "Hello World!" << std::endl;
+    server.start(7777, [](std::shared_ptr<TcpConn> conn)->void {
         std::cout << "成功建立了一个新的连接" << std::endl;
         conn->set_read_cb([conn]()->void {
-            std::cout << "Received: " << conn->get_all_data() << std::endl;
+            std::cout << "Received: " << std::endl << conn->get_all_data() << std::endl;
             conn->send("HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World!", 52);
         });
     });
